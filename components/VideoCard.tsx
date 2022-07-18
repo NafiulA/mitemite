9a230ee91ab6740e457bd0d35c42718ba1165d0a
Nafiul Alam <nafiulalam30@gmail.com>
2022-07-18 23:03:38 +0600

    profile page created

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -38,7 +38,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
             <div>
                 <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
                     <div className='md:w-16 md:h-16 w-10 h-10'>
-                        <Link href='/'>
+                        <Link href={`/profile/${post.postedBy._id}`}>
                             <>
                                 <Image width={62} height={62} className='rounded-full' src={post.postedBy.image} alt="profile image"
                                     layout='responsive' />
@@ -46,7 +46,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                         </Link>
                     </div>
                     <div>
-                        <Link href='/'>
+                        <Link href={`/profile/${post.postedBy._id}`}>
                             <div className='flex items-center gap-2'>
                                 <p className='flex gap-2 items-center md:text-md font-bold text-primary'>{post.postedBy.userName}
                                     <GoVerified className='text-blue-400 text-md'></GoVerified>
