f8a1bd99dc830cde5cb9e574e8db95a877b0b99b
Nafiul Alam <nafiulalam30@gmail.com>
2022-07-20 15:33:13 +0600

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
             <List items={footerList1} mt={false} />
             <List items={footerList2} mt />
             <List items={footerList3} mt />
-            <p className='text-gray-400 text-sm mt-5'>2022 JSM BideoMiru</p>
+            <p className='text-gray-400 text-sm mt-5'>2022 JSM mitemite</p>
         </div>
     )
 }
